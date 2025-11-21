d35d927bb79ecb533470fc733d1eed97d9c5bfd8
Ricky <tcchan71@gmail.com>
2025-11-21 13:43:36 +0000

    added jest type dependency

--- __tests__/seed.test.ts
+++ __tests__/seed.test.ts
@@ -1,11 +1,12 @@
 const db = require('../db/connection');
 const data = require('../db/data/test');
 const seed = require('../db/seeds/seed');
+const { beforeAll } = require('');
 
 beforeAll(() => seed(data));
 
 describe('seed', () => {
   test('users table', () => {
-    return db.select().from(users);
+    return db.select().from();
   });
 });
