0bff17bfbec85e58d1986d1dc665173fcbe22891
Gilson96 <grafael99@gmail.com>
2025-11-25 14:14:13 +0000

    Add DELETE /api/users/:user_id endpoint

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { getScores } from './controllers';
 import { getUser, getUsers } from './controllers/get-controller';
+import { deleteUserId } from './controllers/delete-controller';
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.get('/api/users', getUsers);
 app.get('/api/users/:user_id', getUser);
 app.get('/api/scores', getScores);
 
+app.delete('/api/users/:user_id', deleteUserId);
 export default app;
