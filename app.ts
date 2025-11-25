cddfcdbf165f6b26886cb3ab7ff96b6bb0120ae5
Gilson96 <grafael99@gmail.com>
2025-11-25 13:59:44 +0000

    Add route for get /users/:user_id

--- app.ts
+++ app.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import { getScores } from './controllers';
+import { getUser, getUsers } from './controllers/get-controller';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// app.get('/api/users', findAllUsers);
-
+app.get('/api/users', getUsers);
+app.get('/api/users/:user_id', getUser);
 app.get('/api/scores', getScores);
 
 export default app;
